2d96104a7c64c0891ef9692dca7adab4d114d3b6
inVincible <purohitvedang@gmail.com>
2022-03-20 17:52:16 +1100

    UPDATE: minor fixes, pseudocode completed.

--- text-based-game.py
+++ text-based-game.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 
-class Overhead: # This class contains all overhead in the game (the map and the notepad) as well as other functions.
+class Hud: # This class contains all Hud in the game (the map and the notepad) as well as other functions.
     m_suspects: list # This is a list of suspects where possible suspects can be added as the story progresses. Note that the underscore (_) is implies that this property/method is private and is meant to be used in this class only. Only used as a convention.
     m_notes: list # This list will contain important notes that the player can refer to while playing the game
     m_locations = [" ", " ", " ", " ", " ", " ", " ", " "] # These is a list of possible locations the player wants to be on the map. Limited to 8 locations, 4 in the house and 4 in the workplace.
@@ -19,6 +19,7 @@ class Overhead: # This class contains all overhead in the game (the map and the 
         " ‾‾‾‾\ ‾‾‾‾‾‾ ‾‾‾‾‾‾‾‾‾‾        ‾‾‾‾\‾‾‾‾‾‾‾ ",
         "                                             "
     ]
+
     def WorkMap(self, locations) -> list[str]: # A private function similar to the house map function. It returns the workmap with the player at the desired location.
         return [
          "  Work Place             ",
@@ -50,15 +51,15 @@ class Overhead: # This class contains all overhead in the game (the map and the 
         except ValueError:
             self.m_suspects.append(suspect)
 
-    def printOverhead(self, location: int = 1) -> None: # An essential function that will create, evaluate and print the overhead on the user screen. Takes in the self parameter (provided by python) and a location parameter, which is used to display the player at a specific location.
-        map: list[str] # A scope property initialised a list of strings that will serve as a handler for the map in the overhead.
+    def PrintHud(self, location: int = 1) -> None: # An essential function that will create, evaluate and print the Hud on the user screen. Takes in the self parameter (provided by python) and a location parameter, which is used to display the player at a specific location.
+        map: list[str] # A scope property initialised a list of strings that will serve as a handler for the map in the Hud.
         for i in range(8): self.m_locations[i] = " " # A simple for loop which makes all locations in the _locations property to a string containing a space " " the space represents absence of the player on the map.
         self.m_locations[location] = "@" # Sets the desired location to the player character symbolised by the "@" character
         if(location < 4): # an if-else statement that assigns the scope variable map to a suitable map determined by the location
             map = self.HouseMap(self.m_locations)
         else:
             map = self.WorkMap(self.m_locations)
-        overhead: str = "" # Initialising a string scope variable: This will be whats printed in the end
+        hud: str = "" # Initialising a string scope variable: This will be whats printed in the end
         notepad: list[str] = ["______________________________", # a list of notepad lines. (Could've initialised this in the class to improve performance)
                               "| NOTEPAD:                   |",
                               "|                            |",
@@ -85,16 +86,16 @@ class Overhead: # This class contains all overhead in the game (the map and the 
             notepad.append(suspect)
         notepad.append("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾") # appends a overline to close the notepad
 
-        spaces = "               " # a scope string that physically represents the amount of space between the notepad and the map in the overhead.
-        for i in range(max(len(notepad), len(map))): # a final for loop that loops for the greater of the notepad and map times. It checks whether either notepad or map still have more lines and add them to the overhead variable.
+        spaces = "               " # a scope string that physically represents the amount of space between the notepad and the map in the Hud.
+        for i in range(max(len(notepad), len(map))): # a final for loop that loops for the greater of the notepad and map times. It checks whether either notepad or map still have more lines and add them to the Hud variable.
             if(len(notepad) > i and len(map)) > i:
-                overhead = f"{overhead}{map[i]}{spaces}{notepad[i]}\n"
+                hud = f"{hud}{map[i]}{spaces}{notepad[i]}\n"
             elif(len(notepad) > i):
-                overhead = f"{overhead}{map[-1]}{spaces}{notepad[i]}\n"
+                hud = f"{hud}{map[-1]}{spaces}{notepad[i]}\n"
             else:
-                overhead = f"{overhead}{map[i]}\n"
+                hud = f"{hud}{map[i]}\n"
         
-        print(overhead) # print the overhead string
+        print(hud) # print the Hud string
 
 
 def clear() -> None:
@@ -103,9 +104,9 @@ def clear() -> None:
     else:
         os.system("clear")
 
-score = 75
+score: int = 25
 playerName: str
-overhead: Overhead
+Hud: Hud
 birthdate: str
 knowsAboutWork: bool
 safeUnlocked: bool
@@ -182,7 +183,7 @@ def scene1() -> None:
     validResponse = False
     input("You reach the crime scene, a small well maintained cityside apartment where a young lady of 24 is lying in a pool of blood. Her husband was talking to the police, answering their questions.\n")
     while(not validResponse):
-        overhead.printOverhead(0)
+        Hud.PrintHud(0)
         if(knowsAboutWork):
             a = askQuestion("Do you:", ["Enquire the husband", "Look around the room", "Investigate the body", "Go to the work place"])
             if(a==1):
@@ -218,17 +219,17 @@ def scene2() -> None: # Enquire the husband
     input("I grabbed the spare keys from underneath the decor and opened the door to see this scene. I immediately called the police.")
     input("\nOther police officer: We have enquired other neighbours. Since they live in the heart of the city, no one is usually home before 6pm on a working day. Thus, no one around apart from an old lady next door was at home in this building at the time of this incident.\n")
     print("You write down notes in your notebook.\n")
-    overhead.AddNote("Husband was out.")
+    Hud.AddNote("Husband was out.")
     input(f"{playerName}: Who else has access to your apartment? Anyone that may have the motive to kill your wife?")
     input("\nHusband: Well, the old lady next door is always complaining about how loud our TV is in the evenings. Don't let her age fool you, detective.\n"
     "Despite being a 62 year old woman she smokes and gets wasted all the time." 
     " My wife also worked as a lawyer, so a lot of bad guys she put away may have the motive.")
     global knowsAboutWork
     knowsAboutWork = True
-    overhead.AddNote("Worked as a lawyer.")
+    Hud.AddNote("Worked as a lawyer.")
     validResponse = False
     while(not validResponse):
-        overhead.printOverhead(1)
+        Hud.PrintHud(1)
         a = askQuestion("Do you ", ["Look around the room", "Investigate the body", "Investigate the neighbour", "Go to her workplace", "Arrest the Husband"])
 
         if(a==1):
@@ -255,12 +256,12 @@ def scene3() -> None: # Look around the house
     print("You also find a document that was mostly illegible since there was yogurt spilt all over it. However, you could make out the words: \n"
     "\"Thank you for purchasing LIVELONG life insurance, Mr and Mrs Jackson.\" and the date, which was only 3 days ago.\n")
     input()
-    print(f"{playerName}: Oh, so couple purchased a life insurance 3 days ago. The husband said he was outside for the past week, but perhaps he was plotting her murder.\n")
+    print(f"{playerName}: Oh, so the couple purchased a life insurance policy 3 days ago. The husband said he was outside for the past week, but perhaps he was plotting her murder.\n")
     print("You write down notes in your notepad.\n")
-    overhead.AddNote("Victim had life insurance.")
+    Hud.AddNote("Victim had life insurance.")
     input("You add husband as a suspect.\n")
-    overhead.AddSuspect("Husband")
-    overhead.printOverhead(2)
+    Hud.AddSuspect("Husband")
+    Hud.PrintHud(2)
     if(knowsAboutWork):
         a = askQuestion("Do you: ", ["Enquire the husband", "Investigate the neighbour", "Investigate the body", "Go to her workplace"])
         if(a==1):
@@ -287,10 +288,10 @@ def scene4() -> None: # Investigate the body
     "There are no CCTVs in the building, and their apartment was on the tenth floor, so the murderer should've used the door to make his entrance and exit. There are no fingerprints around, and the murder weapon isn't found")
     input()
     print("You take down notes\n")
-    overhead.AddNote("Time of death: 4:30 pm.")
-    overhead.AddNote("No fingerprints.")
-    overhead.AddNote("No murder weapon found.")
-    overhead.printOverhead(1)
+    Hud.AddNote("Time of death: 4:30 pm.")
+    Hud.AddNote("No fingerprints.")
+    Hud.AddNote("No murder weapon found.")
+    Hud.PrintHud(1)
     if(knowsAboutWork):
         a = askQuestion("Do you: ", ["Enquire the husband", "Investigate the neighbour", "Look around the room", "Go to her workplace"])
         if(a==1):
@@ -322,14 +323,14 @@ def scene5() -> None: # Investigate the neighbour
     input("Old lady: Here is the receipt.")
     print("The officer returns saying he couldn't find the murder weapon, however he found a knife holder for two knives with a missing knife. The knife matched the description of the murder weapon.\n")
     input("You take down notes.\n")
-    overhead.AddNote("Old lady was out at 4:30")
-    overhead.AddNote("Missing knife at neighbour.")
-    overhead.AddNote("Vent connects the 2 houses.")
+    Hud.AddNote("Old lady was out at 4:30")
+    Hud.AddNote("Missing knife at neighbour.")
+    Hud.AddNote("Vent connects the 2 houses.")
     input("You add Old Lady as a suspect.\n")
-    overhead.AddSuspect("Old Lady")
+    Hud.AddSuspect("Old Lady")
     validResponse: bool = False
     while(not validResponse):
-        overhead.printOverhead(3)
+        Hud.PrintHud(3)
         a = askQuestion("Do you ", ["Look around the room", "Investigate the body", "Enquire the husband", "Go to her workplace", "Arrest the Old Lady"])
 
         if(a==1):
@@ -352,7 +353,7 @@ def scene5() -> None: # Investigate the neighbour
 
 def scene6() -> None: # Go to her workplace
     input("You reach a busy law firm where the victim used to work. You decide to explore the place.\n")
-    overhead.printOverhead(4)
+    Hud.PrintHud(4)
     a = askQuestion("Do you: ", ["Visit the Victim's desk", "Enquire her co-worker", "Enquire her boss", "Look around the workplace", "Return to the crime scene"])
     if(a==1):
         scene7() # Visit the Victim's desk
@@ -381,7 +382,7 @@ def scene7() -> None: # Visit the desk
                 if(temp == 2): break
                 continue
             pin += digit
-        overhead.printOverhead(5)
+        Hud.PrintHud(5)
         EnterPIN(pin)
         
     if(safeUnlocked):
@@ -391,13 +392,13 @@ def scene7() -> None: # Visit the desk
         print("\nYou have a close look at the letter on the scribbled handwriting.\n"
         "You recognise a familiar fragrance. The ink on the letter had the scent of a special edition PYLOT pen, one the you used to possess\n")
         input("You take notes\n")
-        overhead.AddNote("Death threats to victim")
-        overhead.AddNote("about internal corruption")
+        Hud.AddNote("Death threats to victim")
+        Hud.AddNote("about internal corruption")
         if(beenToScene8):
-            print("You add co-worker as a suspect.")
-            overhead.AddSuspect("Terry (Co-worker)")
+            print("You add Terry (Co-worker) as a suspect.")
+            Hud.AddSuspect("Terry (Co-worker)")
     
-    overhead.printOverhead(5)
+    Hud.PrintHud(5)
     a = askQuestion("Do you: ", ["Visit the Victim's desk again", "Enquire her coworker", "Enquire her boss", "Look around the workplace", "Return to the crime scene"])
     if(a==1):
         scene7() # Visit the desk again
@@ -417,7 +418,7 @@ def EnterPIN(PIN: str, counter: int = 3) -> None:
         if answer == PIN:
             global safeUnlocked
             safeUnlocked = True
-            return None
+            return
         else:
             counter -= 1
             print(f"The PIN is wrong. You have {counter} {'tries' if counter != 1 else 'try'} remaining.")
@@ -440,10 +441,10 @@ def scene8() -> None: # Enquire her co-worker
     input(f"{playerName}: Thank you for your time. We shall reach back to you if we need to.")
     if(safeUnlocked):
         input("You add Terry as a suspect\n")
-        overhead.AddSuspect("Terry (Co-worker)")
+        Hud.AddSuspect("Terry (Co-worker)")
     validResponse: bool = False
     while(not validResponse):
-        overhead.printOverhead(4)
+        Hud.PrintHud(4)
         a = askQuestion("Do you: ", ["Visit the Victim's desk", "Enquire her boss", "Look around the workplace", "Return to the crime scene", "Arrest the coworker"])
         if(a==1):
             validResponse = True
@@ -479,14 +480,14 @@ def scene9() -> None: # Enquire her boss
     input(f"{playerName}: I see. Thank you a lot for your time.")
 
     input("\n You add Boss as a suspect")
-    overhead.AddSuspect("Boss")
+    Hud.AddSuspect("Boss")
 
     input("\n You add Terry (Co-worker) as a suspect\n")
-    overhead.AddSuspect("Terry (Co-worker)")
+    Hud.AddSuspect("Terry (Co-worker)")
 
     validResponse: bool = False
     while(not validResponse):
-        overhead.printOverhead(6)
+        Hud.PrintHud(6)
         a = askQuestion("Do you: ", ["Visit the Victim's desk", "Enquire her co-worker", "Look around the workplace", "Return to the crime scene", "Arrest the boss"])
         if(a==1):
             validResponse = True
@@ -510,10 +511,10 @@ def scene10() -> None: # Look around the workpalce (and locate the storeroom)
     input("You decode the hexadecimal to ASCII. It reads: \"CORRUPT Cases\"")
     input("It contains a pile of case documents with several notes and highlights. They were all seemed to be about corruption caused by a lawyer called Terry\n")
     input("You take notes\n")
-    overhead.AddNote("Victim had")
-    overhead.AddNote("Evidence against Terry")
+    Hud.AddNote("Victim had")
+    Hud.AddNote("Evidence against Terry")
 
-    overhead.printOverhead(7)
+    Hud.PrintHud(7)
     a = askQuestion("Do you: ", ["Visit the Victim's desk", "Enquire her coworker", "Enquire her boss", "Return to the crime scene"])
     if(a==1):
         scene7() # Visit the desk
@@ -543,7 +544,7 @@ if(__name__ == "__main__"):
     knowsAboutWork = False
     safeUnlocked = False
     beenToScene8 = False
-    overhead = Overhead()
-    overhead.AddNote(f"Victim born on {birthdate}")
+    Hud = Hud()
+    Hud.AddNote(f"Victim born on {birthdate}")
 
     game()
